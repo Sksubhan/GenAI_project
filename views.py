3daee85341ae536b04d2bf54e593dfee8b115ed6
Subhani Shaik <99859528+Sksubhan@users.noreply.github.com>
2024-09-03 10:34:45 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+import google.generativeai as genai
+import markdown
+
+
+def home(request):
+    if request.method == 'POST':
+        # Get the user input from the POST request
+        a = request.POST["txt"]
+
+        # Configure the Generative AI API
+        genai.configure(api_key="AIzaSyCcnyvWjjeBaPlB8kKtkPSOdxX9p72zW8s")  # < --  API from gemini AI.
+        model = genai.GenerativeModel("gemini-pro")
+
+        # Generate content using the AI model
+        response = model.generate_content(f"{a}")
+
+        # Get the text response and format it as a string
+        markdown_text = response.text.replace('•', '*')
+        text = markdown.markdown(markdown_text)
+
+        # Render the response in the 'index.html' template
+        return render(request, 'a.html', {'text': text, 'a': a})
+
+    # If it's a GET request, simply render the empty form
+    return render(request, 'a.html')
